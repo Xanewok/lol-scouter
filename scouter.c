e263c8c9c4c360260584ea4523819656415e7d4b
Xanewok <Xanewok@gmail.com>
2014-04-05 09:31:53 +0200

    Standarizing the indentations, few minor cleanups

--- scouter.c
+++ scouter.c
@@ -1,73 +1,69 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include "cJSON/cJSON.h"
- 
- 
+
 /* http://curl.haxx.se/libcurl/c/curl_easy_setopt.html#CURLOPTWRITEFUNCTION */
 size_t write_func( char *ptr, size_t size, size_t nmemb, void *userdata)
 {
 	printf("%s\n", ptr);
-	printf("size = %d\nnmemb=%d\n", size, nmemb);
-	/* */
 	cJSON *json;
 	char *out;
-	
+
 	json = cJSON_Parse(ptr);
-    if (!json) {
+	if (!json) {
 		printf("Error before: [%s]\n",cJSON_GetErrorPtr());
 	} else {
-        out = cJSON_Print(json);
-        cJSON_Delete(json);
-        printf("%s\n",out);
-        free(out);
+		out = cJSON_Print(json);
+		cJSON_Delete(json);
+		printf("%s\n",out);
+		free(out);
 	}
-	
+
 	return size*nmemb; // return bytes taken care of by func
 }
- 
- 
+
 int main(int argc, char* argv[])
 {
-  printf("This product is not endorsed, certified or otherwise approved in any way by Riot Games, Inc. or any of its affiliates.\n\n"); // we need to put this somewhere else
-  printf("Please input your API key: ");
-  char key[37];
-  scanf("%s", key);
+	printf("This product is not endorsed, certified or otherwise approved in any way by Riot Games, Inc. or any of its affiliates.\n\n"); // we need to put this somewhere else
+	printf("Please input your API key: ");
+	char key[37];
+	scanf("%s", key);
 
-  CURL *curl;
-  CURLcode res;
- 
-  curl_global_init(CURL_GLOBAL_ALL);
+	CURL *curl;
+	CURLcode res;
 
-  const char *test_url = "prod.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/Blessa?api_key=";  
-  char buf[200];
-  sprintf(buf, "%s%s", test_url, key);
-  
- 
+	curl_global_init(CURL_GLOBAL_ALL);
 
-  curl = curl_easy_init();
-  if (curl) {
-    /* Tell libcurl to *not* verify the peer. With libcurl you disable this with */
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-    //curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+	const char *test_url = "prod.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/Blessa?api_key=";
+	char buf[200];
+	sprintf(buf, "%s%s", test_url, key);
+
+
+
+	curl = curl_easy_init();
+	if (curl) {
+		/* Tell libcurl to *not* verify the peer. With libcurl you disable this with */
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+		//curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
 	curl_easy_setopt(curl, CURLOPT_URL, buf);
-	
+
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_func);
-    /* example.com is redirected, so we tell libcurl to follow redirection */ 
-    //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
- 
-    /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
+		/* example.com is redirected, so we tell libcurl to follow redirection */
+		//curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-    /* Check for errors */ 
-    if (res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
-    /* always cleanup */ 
-    curl_easy_cleanup(curl);
-  }
-  
-  curl_global_cleanup();
+		/* Perform the request, res will get the return code */
+		res = curl_easy_perform(curl);
 
-  return 0;
+		/* Check for errors */
+		if (res != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+							curl_easy_strerror(res));
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+	}
+
+	curl_global_cleanup();
+
+	return 0;
 }
